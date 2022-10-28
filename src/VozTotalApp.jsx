4d2196bcccaf8a87adb4ef1458fd7071ec4c423b
SaraChavezCh <sarachavez856@gmail.com>
2022-10-28 12:15:46 -0500

    [ADD] contact page

--- src/VozTotalApp.jsx
+++ src/VozTotalApp.jsx
@@ -1,7 +1,7 @@
 import { HashRouter } from "react-router-dom"
 import { AppRouter } from "./router/AppRouter"
 import { Footer, NavBar } from "./ui/components"
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export const VozTotalApp = () => {
 
       return (
